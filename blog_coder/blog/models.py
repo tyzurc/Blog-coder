28cd85c18a3c4919b0d95fc6874a2d720a2aa0ce
Tyler Cruz <tyzurc97@gmail.com>
2022-04-19 19:14:54 -0300

    Modelo posts creado, registrado y funcionando

--- blog_coder/blog/models.py
+++ blog_coder/blog/models.py
@@ -1,3 +1,16 @@
 from django.db import models
+from datetime import datetime
 
 # Create your models here.
+
+class Posts(models.Model):
+    id = models.AutoField(primary_key=True)
+    title = models.CharField(max_length=40)
+    subtitle = models.CharField(max_length=40)
+    body = models.TextField()
+    author = models.CharField(max_length=40)
+    date = models.DateField(default=datetime.now)
+    image = models.ImageField(upload_to='pictures', null=True, blank=True)
+
+    def __str__(self):
+        return f'{self.title} by {self.author}'
--- blog_coder/blog/views.py
+++ blog_coder/blog/views.py
@@ -1,3 +1,16 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+
+# CVB
+
+from django.views.generic import ListView
+from django.views.generic.detail import DetailView
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
+
+# Django authentication
+
+from django.contrib.auth.forms import AuthenticationForm
+from django.contrib.auth import login, authenticate
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.contrib.auth.decorators import login_required
 
 # Create your views here.
